6fa7b8289721d50af87d6a6e4430f06dc11450b3
Samuel Montalvo <sammontalvojr@gmail.com>
2020-05-26 14:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const shout = (str) => {
+const shout = str => {
   return str.toUpperCase();
 }
 
 const whisper = str => {
   return str.toLowerCase();
+}
+
+const logShout = str => {
+  return console.log(shout(str));
 }
